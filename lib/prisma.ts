bc486638684d170296175031e9176c51960c2ca7
ouattaraissouf7507 <ouattaraissouf7507@gmail.com>
2026-01-22 11:25:11 +0000

    fix: import prismas

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from "@/app/generated/prisma";
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../app/generated/prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
+});
+const prisma = new PrismaClient({ adapter });
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
-const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
-
-export default prisma;
+export { prisma };
+// /
