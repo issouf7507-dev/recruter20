571532cfe66f1b88361dab9489aac458f86fbcbe
ouattaraissouf7507 <ouattaraissouf7507@gmail.com>
2026-04-12 10:38:36 +0000

    sw

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -12,19 +12,3 @@ const adapter = new PrismaMariaDb({
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
-
-// import { PrismaClient } from "@/app/generated/prisma";
-
-// const prismaClientSingleton = () => {
-//   return new PrismaClient();
-// };
-
-// declare global {
-//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-// }
-
-// const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
-
-// export default prisma;
